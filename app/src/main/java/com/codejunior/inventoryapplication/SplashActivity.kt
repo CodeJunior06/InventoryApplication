42c2c9d326fb719f9a4259314ed1ef144834db94
CodeJunior06 <san1428.srp@gmail.com>
2022-08-27 02:35:10 -0300

    IMPLEMENTACION
    - Se agrega Splash Screen
    - Se introduce la libreria Lottie File para animaciones pre diseñadas y la implementacion de View Binding
    - Se quita el Toolbar por defecto
    - Se crea la navegación del Splash Screen a la actividad del Login

--- app/src/main/java/com/codejunior/inventoryapplication/SplashActivity.kt
+++ app/src/main/java/com/codejunior/inventoryapplication/SplashActivity.kt
@@ -1,14 +1,44 @@
 package com.codejunior.inventoryapplication
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.ContentInfoCompat
+import com.codejunior.inventoryapplication.databinding.ActivitySplashBinding
 
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private var _binding: ActivitySplashBinding? = null
+    private val binding get() = _binding
+    private lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        context = this;
+        startTimer()
+
+    }
+
+    private fun startTimer() {
+        object : CountDownTimer(5200, 1500) {
+            override fun onTick(p0: Long) {
+                println("Timer Splash $p0")
+            }
+
+            override fun onFinish() {
+                val i = Intent(context, LoginActivity::class.java)
+                i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+                i.flags =  Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(i)
+                finish()
+            }
+
+        }.start()
     }
 }
