a900ff0cdb10d222443d97bd3f423fa8bd09a1c2
CodeJunior06 <san1428.srp@gmail.com>
2022-09-10 13:53:54 -0500

    IMPLEMENTACION
    Se agregan las dependencias de Dagger Hilt y el view model

---
+++ app/src/main/java/com/codejunior/inventoryapplication/model/UserFirebase.kt
@@ -0,0 +1,4 @@
+package com.codejunior.inventoryapplication.model
+
+class UserFirebase {
+}
--- app/src/main/java/com/codejunior/inventoryapplication/view/LoginActivity.kt
+++ app/src/main/java/com/codejunior/inventoryapplication/view/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.codejunior.inventoryapplication
+package com.codejunior.inventoryapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codejunior.inventoryapplication.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
