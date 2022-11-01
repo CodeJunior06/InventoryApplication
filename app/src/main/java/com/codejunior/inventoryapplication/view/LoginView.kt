77c84087ed40f5b6ca0ece404eab248295df237a
Sebastian Ramirez <87379161+JuanRamirezMendoza@users.noreply.github.com>
2022-11-01 16:43:59 -0500

    nestedScroll
    se implementa scroll en la vista del login

--- app/src/main/java/com/codejunior/inventoryapplication/view/LoginView.kt
+++ app/src/main/java/com/codejunior/inventoryapplication/view/LoginView.kt
@@ -2,13 +2,19 @@ package com.codejunior.inventoryapplication.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.ScrollView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
 import com.codejunior.inventoryapplication.databinding.ActivityLoginBinding
 import com.codejunior.inventoryapplication.utils.extension.toastMessage
 import com.codejunior.inventoryapplication.viewmodel.*
-
+import com.codejunior.inventoryapplication.window.WindowUtils
+import com.codejunior.inventoryapplication.window.WindowUtils.Companion.showSoftKeyboardSetup
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class LoginView : AppCompatActivity() {
@@ -19,7 +25,7 @@ class LoginView : AppCompatActivity() {
     }
 
     private var _binding: ActivityLoginBinding? = null
-    private val binding get() =  _binding
+    private val binding get() =  _binding!!
     private val loginViewModel: LoginViewModel by viewModels()
 
     private val context = this
@@ -27,9 +33,11 @@ class LoginView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-        binding!!.lifecycleOwner = this
-        binding!!.viewModelLogin = loginViewModel
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
+        binding.viewModelLogin = loginViewModel
+
+        showSoftKeyboardSetup(binding.root)
 
         loginViewModel.error.observe(this) {
             toastMessage(it)
---
+++ app/src/main/java/com/codejunior/inventoryapplication/window/WindowUtils.kt
@@ -0,0 +1,42 @@
+package com.codejunior.inventoryapplication.window
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBinding
+
+class WindowUtils {
+    companion object {
+        @SuppressLint("InternalInsetResource", "DiscouragedApi")
+        @JvmStatic
+        fun Context.getDimenByName(name: String): Int {
+            var result = 0
+            val resourceId = resources.getIdentifier(name, "dimen", "android")
+            if (resourceId > 0) {
+                result = resources.getDimensionPixelSize(resourceId)
+            }
+            return result
+        }
+
+        @JvmStatic
+        fun ViewBinding.fillMarginWhitSetDecorFitsSystemWindows(container: View? = null) {
+            (container ?: root).setPadding(
+                0,
+                root.context.getDimenByName("status_bar_height"),
+                0,
+                root.context.getDimenByName("navigation_bar_height")
+            )
+        }
+
+        @JvmStatic
+        fun showSoftKeyboardSetup(view: View) {
+            val deferringInsetsListener = RootViewDeferringInsetsCallback(
+                persistentInsetTypes = WindowInsetsCompat.Type.systemBars(),
+                deferredInsetTypes = WindowInsetsCompat.Type.ime()
+            )
+            ViewCompat.setOnApplyWindowInsetsListener(view, deferringInsetsListener)
+        }
+    }
+}
